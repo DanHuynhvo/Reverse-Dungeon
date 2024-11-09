bdfb8cbc1b2cc2597dffd68d63be90003f9bc410
DanHuynhvo <danqhuynhvo@gmail.com>
2024-11-09 09:30:30 -0800

    Texture Swap for rooms
    Now you can change textures for walls and floors without manually doing it

--- Assets/Scripts/TextureSwap.cs
+++ Assets/Scripts/TextureSwap.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class TextureSwap : MonoBehaviour
 {
-    public SpriteRenderer tileSprite;
+    public Sprite tileSprite;
+    public Sprite wallSprite;
+    public GameObject room;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < room.transform.childCount; i++)
+        {
+
+            if (room.transform.GetChild(i).gameObject.tag == "Floor")
+            {
+                room.transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().sprite = tileSprite;
+            }
+
+            else
+            {
+                room.transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().sprite = wallSprite;
+            }
+        }
+    }
 }
