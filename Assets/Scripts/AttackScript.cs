5211ae7b9217597175ce46a2d4f702e069edb30f
DanHuynhvo <danqhuynhvo@gmail.com>
2024-11-09 19:04:01 -0800

    asfdgd

---
+++ Assets/Scripts/AttackScript.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+    List<Attack> playerAttacks = new List<Attack>();
+
+
+
+    public void chosenAttack(int option)
+    {
+        
+    }
+}
