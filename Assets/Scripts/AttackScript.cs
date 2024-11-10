32b18ab5b79249b78f297f1a998eb620ce1d10b0
DanHuynhvo <danqhuynhvo@gmail.com>
2024-11-09 19:41:46 -0800

    adesrfdf

--- Assets/Scripts/AttackScript.cs
+++ Assets/Scripts/AttackScript.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class AttackScript : MonoBehaviour
 {
     public List<Attack> playerAttacks = new List<Attack>();
+<<<<<<< Updated upstream
+=======
+    public List<GameObject> enemiesInRange = new List<GameObject>();
+    public Transform player = Player.instance.transform;
+    public Vector2 range = Vector2.zero;
+    public LayerMask enemy;
+    GameObject attackChosen;
+>>>>>>> Stashed changes
 
 
 
     public void chosenAttack(int option)
     {
-        
+        attackChosen = playerAttacks[option].gameObject;
+        range = playerAttacks[option].range;
+
+        //Instantiate(attackChosen);
+        enemiesInRange = new List<GameObject>();
+        {
+
+        };
+        Physics2D.OverlapBoxAll(player.position, range, 0, enemy);
+
+
+
     }
 }
