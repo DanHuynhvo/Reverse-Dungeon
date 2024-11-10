556f910fdcf4cd320f2dc22457803efd8179e9a1
JustinRenner <justinrenner52@gmail.com>
2024-11-09 21:29:56 -0800

    Here

--- Assets/Scripts2.0/ICharacter.cs
+++
@@ -1,9 +0,0 @@
-public interface ICharacter
-{
-    bool IsPlayer { get; }
-    int Defense { get; }
-
-    void StartTurn();
-    void TakeAction(); // This method needs to be implemented in PlayerCharacter
-    void TakeDamage(int amount);
-}
