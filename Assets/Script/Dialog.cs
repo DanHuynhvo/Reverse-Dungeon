c5c8e6d4279d7670235954e89f6d88846b3f7f02
JustinRenner <justinrenner52@gmail.com>
2024-11-09 23:43:57 -0800

    Push

--- Assets/Script/Dialog.cs
+++ Assets/Script/Dialog.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Dialog", menuName = "Scriptable Objects/Dialog")]
+[CreateAssetMenu(fileName = "Dialog", menuName = "Dialogue/Line",order = 1)]
 public class Dialog : ScriptableObject
 {
-    
+    public string speakerName;
+    [TextArea(3, 10)]
+    public string line;
+    public Dialog[] choices; // Next lines based on choices
 }
